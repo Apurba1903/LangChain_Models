f547ff1282f1d848f3cce578565929b1faec593b
Apurba1903 <apurba1903@gmail.com>
2026-02-02 06:07:24 +0600

    reuseable template and invoke system using langchain

--- 4.Prompts/prompt_ui.py
+++ 4.Prompts/prompt_ui.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 model = ChatOpenAI()
@@ -38,32 +38,18 @@ length_input = st.selectbox(
     ]
 )
 
-# Template
-template = PromptTemplate(
-    template="""
-            Please summarize the research paper titled \"{paper_input}\" with the following specifications:
-            Explanation Style: {style_input}
-            Explanation Length: {length_input}  
-            1. Mathematical Details: 
-            - Include relevant mathematical equations if present in the paper. 
-            - Explain the mathematical concepts using simple, intuitive code snippets where applicable. 
-            2. Analogies:  
-            - Use relatable analogies to simplify complex ideas.  
-            If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-            Ensure the summary is clear, accurate, and aligned with the provided style and length.
-    """,
-    input_variables=['paper_input', 'style_input', 'length_input']
-)
-
-# Fill the Placeholders
-prompt = template.invoke({
-    'paper_input':paper_input,
-    'style_input':style_input,
-    'length_input':length_input
-})
-
+template = load_prompt('C:/Users/ACER/Desktop/DSMP1/LangChain/4.Prompts/template.json')
 
 
 if st.button('Summarize'):
-    result = model.invoke('prompt')
+    chain = template | model
+    chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
     st.write(result.content)
+
+
+
+
