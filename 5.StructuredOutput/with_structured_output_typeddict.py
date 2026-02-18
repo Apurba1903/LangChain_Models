7c1ac4ffc452232d4c6f6cfecb60745705f18eca
Apurba1903 <apurba1903@gmail.com>
2026-02-18 17:58:59 +0600

    23:42

---
+++ 5.StructuredOutput/with_structured_output_typeddict.py
@@ -0,0 +1,16 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import TypedDict
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+# Schema
+class Review(TypedDict):
+    
+    summary: str
+    sentiment: str
+
+
+result = model.invoke(""" Review """)
