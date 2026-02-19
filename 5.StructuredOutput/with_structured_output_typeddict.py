bdba04fb0cbd6561c811f8a0b111815707ba9af0
Apurba1903 <apurba1903@gmail.com>
2026-02-19 15:11:29 +0600

    fixed with huggingface llms

--- 5.StructuredOutput/with_structured_output_typeddict.py
+++ 5.StructuredOutput/with_structured_output_typeddict.py
@@ -1,10 +1,17 @@
-from langchain_openai import ChatOpenAI
+from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface.llms import HuggingFaceEndpoint
 from dotenv import load_dotenv
 from typing import TypedDict
 
 load_dotenv()
 
-model = ChatOpenAI()
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1",
+    temperature=0.7,
+    max_new_tokens=512
+)
+
+model = ChatHuggingFace(llm=llm)
 
 # Schema
 class Review(TypedDict):
@@ -12,5 +19,8 @@ class Review(TypedDict):
     summary: str
     sentiment: str
 
+structured_model = model.with_structured_output(Review)
 
-result = model.invoke(""" Review """)
+result = structured_model.invoke(""" The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this. """)
+
+print(result)
